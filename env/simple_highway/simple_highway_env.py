adf52f9d96b0b8f7880872f374fda3152ed62a26
melih.dal <melih.dal@eatron.com>
2020-06-16 23:15:27 +0300

    import ptan locally.

--- env/simple_highway/simple_highway_env.py
+++ env/simple_highway/simple_highway_env.py
@@ -182,7 +182,7 @@ class SimpleHighway(gym.Env):
             desired_v = vehcl._desired_v
             
             self._init_vehicle_info.append([lane_id,init_pos_y,desired_v])
-
+            
             vehcl._AIController = AIController(vehcl,
                                                self._vehicles,
                                                self._mode,
@@ -201,155 +201,23 @@ class SimpleHighway(gym.Env):
     def get_vehicle_with_id(self, vehcl_id):
         return self._vehicles[vehcl_id]
 
-    def relative_pos_to_bin(self,rel_pos):
-        out = 1.0
-        if abs(rel_pos) > 27:
-            out = 1.0 # far
-        elif abs(rel_pos) > 11:
-            out = 0.6 # nomina;
-        elif abs(rel_pos) >=0.0:
-            out = 0.2 #near
-        if(rel_pos==0): rel_pos = 0.0001
-        sign = rel_pos/abs(rel_pos)
-        out = sign*out
-        return out
-
-    def relative_vel_to_bin(self,rel_v):
-        out = 0.0
-        if abs(rel_v) > 0.36:
-            out = 1.0 # approach or move away
-        elif abs(rel_v) >= 0.0:
-            out = 0.0 # stable;
-        
-        if rel_v == 0.0: rel_v = 0.00001
-        sign = rel_v/abs(rel_v)
-        out = sign*out
-        return out
-
-    def is_index_valid(self,in_index,in_list):
-        if(in_index<0):
-            return False
-        elif(in_index >= len(in_list)):
-            return False
-        else:
-            return True
-    
-    def get_index_of_front_and_back(self,lane_lst,ego_pos_x):
-        front_index = -1
-        rear_index = -1
-        if(len(lane_lst)>0):
-            MAX_NUM = 5e9
-            diff_list = [MAX_NUM if i-ego_pos_x < 0 else i-ego_pos_x for i in lane_lst]
-            min_val = min(diff_list)
-            if min_val == MAX_NUM:
-                # no front vehicle
-                rear_index = len(lane_lst)
-            else:
-                front_index = diff_list.index(min_val)
-                rear_index = front_index - 1
-        return front_index,rear_index
-
-    def get_obs_x_v(self, front_index,lane_lst,ego_pos_x,ego_speed,pos_vehicle_map):
-        obs_rel_x = 1.0
-        obs_rel_v = 1.0
-        if self.is_index_valid(front_index,lane_lst):
-            agent_pos_x = lane_lst[front_index]
-            rel_pos = agent_pos_x - ego_pos_x
-            obs_rel_x = self.relative_pos_to_bin(rel_pos)
-
-            agent_ref = pos_vehicle_map[agent_pos_x]
-            agent_speed = agent_ref._velocity
-            rel_vel = agent_speed - ego_speed
-            obs_rel_v = self.relative_vel_to_bin(rel_vel) 
-        return obs_rel_x,obs_rel_v
-
-    def get_input_states(self):
-
-        """ 
-        sort all the vehicles in order of their positions on the highway
-
-        the location matrix is then in the form
-
-        also, create a dictionary on the form 
-
-        pos_vehicle_map = {'position':'vehicle_ref'}
-
-        for each lane, there is a list of sorted vectors
-
-
-        Create observation from these matrixes. 
-
-        • Relative position and velocity of the car in front on the
-        same lane (lane l).
-        • Relative position and velocity of the car in front on the
-        left lane (lane l +1).
-        • Relative position and velocity of the car in rear on the
-        left lane (lane l +1).
-        • Relative position and velocity of the car in front on the
-        right lane (lane l −1).
-        • Relative position and velocity of the car in rear on the
-        right lane (lane l −1).
-        • Own lane number (l) 
-
-        Observation length  = 2*3*2 + 1 = 13
-        
-        """ 
-
-
-        lane_pos_list = []
-        pos_vehicle_map = {}
-        for i in range(self.num_lane):
-            lane_pos_list.append([])
-                
-        for vehcl in self._vehicles:
-            lane = vehcl._current_lane
-            pos = vehcl._position
-            pos_x = pos[1]
-            if pos_x not in pos_vehicle_map:
-                pos_vehicle_map[pos_x] = vehcl
-            else:
-                print("two equal pos_x vals found")
-            lane_pos_list[lane].append(pos_x)
-
-        ## sort all the columns
-        for lst in lane_pos_list: lst.sort()
-
-        # fill observation with default values
-        obs = np.full((13, ), 1)
-
+    def get_input_states(self,lane_pos_list,pos_vehicle_map):
         # find front agent
         ego_vehicle =  self._vehicles[self._ego_id]
         ego_lane = ego_vehicle._current_lane
         ego_pos_x = ego_vehicle._position[1]
-        ego_speed = ego_vehicle._velocity # the name _velocity should have been speed
         index_in_lane = lane_pos_list[ego_lane].index(ego_pos_x)
         l_front = index_in_lane  + 1
-        l_rear = index_in_lane - 1
         ego_lane_list = lane_pos_list[ego_lane]
 
-        obs[0],obs[1] = self.get_obs_x_v(l_front,ego_lane_list,ego_pos_x,ego_speed,pos_vehicle_map)
-        obs[2],obs[3] = self.get_obs_x_v(l_rear,ego_lane_list,ego_pos_x,ego_speed,pos_vehicle_map)
-
-        if ego_lane < self.num_lane-1:
-            right_lane = lane_pos_list[ego_lane + 1]
-            l_right_front,l_right_rear = self.get_index_of_front_and_back(right_lane,ego_pos_x)
-            obs[4],obs[5] = self.get_obs_x_v(l_right_front,ego_lane_list,ego_pos_x,ego_speed,pos_vehicle_map)
-            obs[6],obs[7] = self.get_obs_x_v(l_right_rear,ego_lane_list,ego_pos_x,ego_speed,pos_vehicle_map)   
-
-        if ego_lane > 0:
-            left_lane = lane_pos_list[ego_lane - 1]
-            l_left_front,l_left_rear = self.get_index_of_front_and_back(left_lane,ego_pos_x)
-            obs[8],obs[9] = self.get_obs_x_v(l_left_front,ego_lane_list,ego_pos_x,ego_speed,pos_vehicle_map)
-            obs[10],obs[11] = self.get_obs_x_v(l_left_rear,ego_lane_list,ego_pos_x,ego_speed,pos_vehicle_map)   
-
-        obs[12] = ego_lane
+        observation = AIController.level_k_observation(ego_vehicle,self.num_lane,lane_pos_list,pos_vehicle_map)
 
         ego_lead_relative = FAR_AWAY
-        if self.is_index_valid(l_front,ego_lane_list):
+        if AIController.is_index_valid(l_front,ego_lane_list):
             lead_pos_x = ego_lane_list[l_front] 
             ego_lead_relative = lead_pos_x - ego_pos_x
         
-        return obs,ego_lead_relative
+        return observation,ego_lead_relative
 
     # PyGame related function.
     def terminate(self):
@@ -419,7 +287,7 @@ class SimpleHighway(gym.Env):
         return out
 
 
-    def calc_comfort_reard(self,action):
+    def calc_comfort_reward(self,action):
         out = 0
         if action == DriverAction.Maintain:
             out = 0
@@ -474,7 +342,7 @@ class SimpleHighway(gym.Env):
 
         dist_reward = self.lead_distance_to_reward(ego_lead_relative)
 
-        e_comf_reward = self.calc_comfort_reard(action)
+        e_comf_reward = self.calc_comfort_reward(action)
 
         final_reward = np.dot(self.reward_coefs,
             np.array([collision_reward, speed_reward, dist_reward, e_comf_reward]))
@@ -485,7 +353,29 @@ class SimpleHighway(gym.Env):
         return is_done,final_reward
 
 
+    def get_common_state(self):
+        lane_pos_list = []
+        pos_vehicle_map = {}
+        for i in range(self.num_lane):
+            lane_pos_list.append([])
+                
+        for vehcl in self._vehicles:
+            lane = vehcl._current_lane
+            pos = vehcl._position
+            pos_x = pos[1]
+            if pos_x not in pos_vehicle_map:
+                pos_vehicle_map[pos_x] = vehcl
+            else:
+                print("two equal pos_x vals found")
+            lane_pos_list[lane].append(pos_x)
+
+        ## sort all the columns
+        for lst in lane_pos_list: lst.sort()
+
+        return lane_pos_list,pos_vehicle_map
+
     def step(self, action):
+        #print("Step num:", self._steps)
         # find fron vehicles then calculate delta x, v ,a
         for vehcl in self._vehicles:
             vehcl._delta_v, \
@@ -496,21 +386,28 @@ class SimpleHighway(gym.Env):
         lane_change_complete = 0
         i = 0
         action_init = action
+
+
+
         while lane_change_lock and i < 30:
             i = i + 1
+
+            # All vehicles take the state of highway and create own obervation
+            # Collect the common information to save computation time
+            highway_lane_pos_list,pos_vehicle_map = self.get_common_state()
+
             # AIControlller calculates acceleration and lane change decision.
             for vehcl in self._vehicles:
                 if vehcl._is_ego is not True:
                     # get the action according to the policy 
                     # for now olny Level_0 policy exists
-
-                    vehcl._AIController.control(0)
+                    vehcl._AIController.control(0,highway_lane_pos_list,pos_vehicle_map)
                 else:
                     # action is needed to call lane change for EGO
                     if lane_change_complete or i > 15:
                         # this is to force to go straight after completing a lane change
                         action = 0
-                    vehcl._AIController.control(action)
+                    vehcl._AIController.control(action,highway_lane_pos_list,pos_vehicle_map)
 
                 if vehcl._is_lane_changing:
                     # Update the position and heading angle.
@@ -535,14 +432,18 @@ class SimpleHighway(gym.Env):
                 else:
                     if(self.DEBUG_LIMITS):
                         print("Speed limit reached for agent ID %d, speed is: %2.1f"%(vehcl._id,vehcl._velocity))
+            
             self._time = self._time + self._dt
             # Updating the visual environment of the simulation
             if self._mode._is_rendering:
                 self._display.env_update(self._reward_total)
+            
             if not ego_veh._is_lane_changing and i >= 30:
                 lane_change_lock = 0
 
-        observation,ego_lead_relative = self.get_input_states()
+        highway_lane_pos_list,pos_vehicle_map = self.get_common_state()
+
+        observation,ego_lead_relative = self.get_input_states(highway_lane_pos_list,pos_vehicle_map)
         is_done,reward = self.calculate_reward(ego_veh, action_init,ego_lead_relative==ego_lead_relative)
         
         self._reward = reward
